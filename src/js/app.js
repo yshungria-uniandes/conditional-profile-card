dab7dabe4e8a2c658dce55c4e422c6742af9cc53
Yojan S. Hungría <ys.hungria@uniandes.edu.co>
2024-07-31 04:07:48 +0000

    Conditional Profile Card Generator finished

--- src/js/app.js
+++ src/js/app.js
@@ -40,7 +40,7 @@ function render(variables = {}) {
             <li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/school/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
