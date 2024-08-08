c0c0178fbf4d7888494df11ba17936daf08270e4
Yojan S. Hungría <ys.hungria@uniandes.edu.co>
2024-08-08 02:15:22 +0000

    Corrected Nulls .

--- src/js/app.js
+++ src/js/app.js
@@ -23,47 +23,42 @@ import "../style/index.css";
     }
  */
 function render(variables = {}) {
-  console.log("These are the current variables: ", variables); // print on the console
+  console.log("These are the current variables: ", variables); //print on the console
   // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
-  // Create social media links if the usernames are provided
-  let twitterLink = variables.twitter
-    ? `<li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>`
-    : "";
-  let githubLink = variables.github
-    ? `<li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>`
-    : "";
-  let linkedinLink = variables.linkedin
-    ? `<li><a href="https://linkedin.com/in/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>`
-    : "";
-  let instagramLink = variables.instagram
-    ? `<li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>`
-    : "";
-
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name ? variables.name : "Bob"} ${
-    variables.lastName ? variables.lastName : "Dylan"
-  }</h1>
-          <h2>${variables.role ? variables.role : "Unemployed"}</h2>
-          <h3>${variables.city ? variables.city : "Somewhere"}, ${
-    variables.country ? variables.country : "Somewhere"
-  }</h3>
-          <ul class="${variables.socialMediaPosition}">
-            ${twitterLink}
-            ${githubLink}
-            ${linkedinLink}
-            ${instagramLink}
-          </ul>
-        </div>
-    `;
+                ${cover}
+              <img src="${variables.avatarURL}" class="photo" />
+              <h1>
+              ${variables.name ? variables.name : "Your name"}
+              ${variables.lastName ? variables.lastName : "Your lastname"}
+              </h1>
+              <h2>${variables.role ? variables.role : "Web developer"}</h2>
+              <h3>${variables.city ? variables.city : "Miami,"}
+                  ${variables.country ? variables.country : "USA"}
+              </h3>
+              <ul class=${variables.socialMediaPosition}>
+                <li><a href="https://twitter.com/${
+                  variables.twitter
+                }"><i class="fab fa-twitter"></i></a></li>
+                <li><a href="https://github.com/${
+                  variables.github
+                }"><i class="fab fa-github"></i></a></li>
+                <li><a href="https://linkedin.com/${
+                  variables.linkedin
+                }"><i class="fab fa-linkedin"></i></a></li>
+                <li><a href="https://instagram.com/${
+                  variables.instagram
+                }"><i class="fab fa-instagram"></i></a></li>
+              </ul>
+    
+            </div>
+        `;
 }
-
 /**
  * Don't change any of the lines below, here is where we do the logic for the dropdowns
  */
@@ -76,7 +71,7 @@ window.onload = function() {
     // this is the url for the profile avatar
     avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
     // social media bar position (left or right)
-    socialMediaPosition: "right",
+    socialMediaPosition: "position-left",
     // social media usernames
     twitter: null,
     github: null,
